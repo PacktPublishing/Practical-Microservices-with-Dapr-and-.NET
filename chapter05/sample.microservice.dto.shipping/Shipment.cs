fdca65af8fefda9d3f70af44697baa6829af6d96
Davide Bedin <davide.bedin@outlook.com>
2020-10-25 18:21:10 +0100

    chapter 5 fixes

---
+++ chapter05/sample.microservice.dto.shipping/Shipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sample.microservice.dto.shipping
+{
+    public class Shipment
+    {
+        public DateTime Date { get; set; }
+
+        public Guid OrderId { get; set; }
+
+        public string CustomerCode { get; set; }
+
+        public int NumberOfItems { get; set; }
+    }
+}
